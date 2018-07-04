6738a97c9a487317f5acf736e9686263eb444733
p.mierkowski <p.mierkowski@gmail.com>
2018-07-04 20:53:42 +0200

    Initial commit

---
+++ src/main/java/pl/pmierkowski/bookssearch/BooksSearchApplication.java
@@ -0,0 +1,12 @@
+package pl.pmierkowski.bookssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksSearchApplication.class, args);
+    }
+}
