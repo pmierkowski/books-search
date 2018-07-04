d894cc4948c06512c777eca93f7efe8eededac81
p.mierkowski <p.mierkowski@gmail.com>
2018-07-04 21:09:17 +0200

    Added base view

--- src/main/java/pl/pmierkowski/bookssearch/BooksSearchApplication.java
+++ src/main/java/pl/pmierkowski/bookssearch/BooksSearchApplication.java
@@ -2,7 +2,9 @@ package pl.pmierkowski.bookssearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "pl.pmierkowski.bookssearch")
 @SpringBootApplication
 public class BooksSearchApplication {
 
---
+++ src/main/java/pl/pmierkowski/bookssearch/controller/BooksSearchController.java
@@ -0,0 +1,13 @@
+package pl.pmierkowski.bookssearch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BooksSearchController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
